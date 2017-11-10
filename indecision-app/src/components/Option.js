be7560dd5217a0522dbb56de3101d2311f0a0e4c
Jaime Farnan <jaime.farnan@us.dunnhumby.com>
2017-11-10 14:23:26 -0500

    wrapping up part 1 indecision app now with styles

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 const Option = (props) => (
 // export default (props) => { - can do something like this, but <Unknown> will show in Developer tools
-  <div>
-    <p>
-      {props.optionText}
-      <button
-        onClick={(e) => {
-          // we don't want to pass the e argument up, so we create this function to pass up what we actually want
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        remove
-      </button>
+  <div className="option">
+    <p className="option__text">
+      {props.count}. {props.optionText}
     </p>
+    <button
+      className="button button--link"
+      onClick={(e) => {
+        // we don't want to pass the e argument up, so we create this function to pass up what we actually want
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      remove
+    </button>
   </div>
 );
 
--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -10,10 +10,14 @@ const OptionModal = (props) => (
     onRequestClose={props.handleClearSelectedOption}
     // this shows for users with accessibility enabled - does not show in browser
     contentLabel="Selected Option"
+    // How long should the modal wait before it closes?
+    closeTimeoutMS={200}
+    // className is another prop on ReactModal where we can override the modal styles
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
 
